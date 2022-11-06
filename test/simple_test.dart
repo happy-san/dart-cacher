da9cdb803ae101954e47ea3f8b1cccdf468fa057
harpreet-applify <happy_san@protonmail.com>
2022-11-06 11:08:28 +0530

    Lint changes.

--- test/simple_test.dart
+++ test/simple_test.dart
@@ -10,7 +10,7 @@ void main() {
   });
 
   test('Test simple insert/get', () {
-    Cache<
+    final Cache<
         InMemoryStorage<CacheEntry<String, int>, String, int>,
         CacheEntry<String, int>,
         String,
@@ -20,20 +20,22 @@ void main() {
     expect(c.get('key'), equals(42));
   });
   test('Test simple loader function', () {
-    SimpleCache<int, int> c = SimpleCache<int, int>(storage: SimpleStorage(20))
-      ..loader = (int k, _) => k * 10;
+    final SimpleCache<int, int> c =
+        SimpleCache<int, int>(storage: SimpleStorage(20))
+          ..loader = (int k, _) => k * 10;
     expect(c.get(4), equals(40));
     expect(c.get(5), equals(50));
   });
   test('Test simple loader function', () {
-    SimpleCache<int, int> c = SimpleCache<int, int>(storage: SimpleStorage(20))
-      ..syncLoading = true
-      ..expiration = const Duration(milliseconds: 200)
-      ..loader = (int k, int? oldValue) {
-        oldValue ??= k;
-        var v = oldValue * 10;
-        return v;
-      };
+    final SimpleCache<int, int> c =
+        SimpleCache<int, int>(storage: SimpleStorage(20))
+          ..syncLoading = true
+          ..expiration = const Duration(milliseconds: 200)
+          ..loader = (int k, int? oldValue) {
+            oldValue ??= k;
+            final v = oldValue * 10;
+            return v;
+          };
 
     expect(c.get(4), equals(40));
     expect(c.get(4), equals(40));
@@ -42,14 +44,15 @@ void main() {
     expect(c.get(4), equals(400));
   });
   test('Test simple loader function', () {
-    SimpleCache<int, int> c = SimpleCache<int, int>(storage: SimpleStorage(20))
-      ..syncLoading = false
-      ..expiration = const Duration(milliseconds: 200)
-      ..loader = (int k, int? oldValue) {
-        oldValue ??= k;
-        var v = oldValue * 10;
-        return v;
-      };
+    final SimpleCache<int, int> c =
+        SimpleCache<int, int>(storage: SimpleStorage(20))
+          ..syncLoading = false
+          ..expiration = const Duration(milliseconds: 200)
+          ..loader = (int k, int? oldValue) {
+            oldValue ??= k;
+            final v = oldValue * 10;
+            return v;
+          };
 
     expect(c.get(4), equals(40));
     expect(c.get(4), equals(40));
@@ -58,24 +61,26 @@ void main() {
     expect(c.get(4), equals(40));
   });
   test('Test simple async loader function', () async {
-    SimpleCache<int, int> c = SimpleCache<int, int>(storage: SimpleStorage(20))
-      ..syncLoading = false
-      ..expiration = const Duration(milliseconds: 200)
-      ..loader = (int k, int? oldValue) async {
-        oldValue ??= k;
-        var v = oldValue * 10;
-        return v;
-      };
+    final SimpleCache<int, int> c =
+        SimpleCache<int, int>(storage: SimpleStorage(20))
+          ..syncLoading = false
+          ..expiration = const Duration(milliseconds: 200)
+          ..loader = (int k, int? oldValue) async {
+            oldValue ??= k;
+            final v = oldValue * 10;
+            return v;
+          };
 
     expect(c.get(4), equals(null));
     await Future<dynamic>.delayed(Duration(seconds: 1));
     expect(c.get(4), equals(40));
   });
   test('Test simple eviction', () {
-    SimpleCache<int, int> c = SimpleCache<int, int>(storage: SimpleStorage(3))
-      ..loader = (int k, _) {
-        return k * 10;
-      };
+    final SimpleCache<int, int> c =
+        SimpleCache<int, int>(storage: SimpleStorage(3))
+          ..loader = (int k, _) {
+            return k * 10;
+          };
 
     expect(c.get(4), equals(40));
     expect(c.get(5), equals(50));
@@ -84,7 +89,7 @@ void main() {
     expect(c.containsKey(4), equals(false));
   });
   test('Test LRU eviction', () async {
-    LruCache<int, int> c = LruCache<int, int>(storage: LruStorage(3))
+    final LruCache<int, int> c = LruCache<int, int>(storage: LruStorage(3))
       ..loader = (int k, _) {
         return k * 10;
       };
@@ -99,7 +104,7 @@ void main() {
     expect(c.containsKey(5), equals(false));
   });
   test('Test LFU eviction', () {
-    LfuCache<int, int> c = LfuCache<int, int>(storage: LfuStorage(3))
+    final LfuCache<int, int> c = LfuCache<int, int>(storage: LfuStorage(3))
       ..loader = (int k, _) {
         return k * 10;
       };
